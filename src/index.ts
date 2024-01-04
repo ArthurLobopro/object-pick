0f094f72dd59a1d18577802376722a8e17513a8e
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-01-04 12:13:46 -0300

    feat: first version

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+function cloneValue<T>(value: T): T {
+    if (typeof value === "object") {
+        if (Array.isArray(value)) {
+            return cloneArray(value) as T
+        }
+
+        if (value !== null) {
+            return cloneObject(value) as T
+        }
+    }
+
+    return value
+}
+
+function cloneArray<T extends any[]>(array: T): T {
+    return array.map(cloneValue) as T
+}
+
+function cloneObject<T extends object>(obj: T): T {
+    return Object.fromEntries(
+        Object.entries(obj)
+            .map(cloneValue)
+    ) as T
+}
+
+type untypedObject = { [key: string]: any }
+type PickProperties<T, K extends keyof T> = {
+    [P in K]: T[P]
+}
+
+type OmitProperties<T, K extends keyof T> = {
+    [P in Exclude<keyof T, K>]: T[P]
+}
+
+export function pick<T extends untypedObject, K extends keyof T>(obj: T, props: K[]) {
+    return Object.fromEntries(
+        Object.entries(obj)
+            .filter(([key]) => props.includes(key as K))
+            .map((([key, value]) => [key, cloneValue(value)]))
+    ) as PickProperties<T, K>
+}
+
+export function pickAllExcept<T extends untypedObject, K extends keyof T>(obj: T, props: K[]) {
+    return Object.fromEntries(
+        Object.entries(obj)
+            .filter(([key]) => !props.includes(key as K))
+            .map((([key, value]) => [key, cloneValue(value)]))
+    ) as OmitProperties<T, K>
+}
+
+export const pickAll = cloneObject
