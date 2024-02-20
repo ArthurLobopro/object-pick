6cc6603a30abef6e2c4b5a7a9e02298a0de1aa4a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-02-20 12:45:33 -0300

    feat: create tests

---
+++ src/index.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from "@jest/globals"
+import { clone, pick, pickAll } from "."
+
+const obj = {
+    a: "a",
+    b: [1, 2, 3]
+}
+
+test("Picking Properties", () => {
+    expect(pick(obj, ["b"])).toEqual({ b: [1, 2, 3] })
+})
+
+test("Clonning object", () => {
+    expect(pickAll(obj)).toEqual(obj)
+})
+
+test("Clone value", () => {
+    expect(clone(obj)).toEqual(obj)
+    expect(clone("a")).toEqual("a")
+    expect(clone(10)).toEqual(10)
+    expect(clone([obj, "a", 10])).toEqual([obj, "a", 10])
+})
+
+test("Random data api test", () => {
+    return fetch("https://random-data-api.com/api/users/random_user")
+        .then((data) => data.json())
+        .then((fakeData: object) => {
+            expect(clone(fakeData)).toEqual(fakeData)
+        })
+})
